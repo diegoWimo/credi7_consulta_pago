572f1fbf19f29b08e5874bac52b5835500332958
legolasblack <legolas.black.ds@gmail.com>
2023-01-03 18:30:04 -0600

    primer index

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>Welcome to back</title>
+    <style>
+    .blue{
+        background: #0080BD;
+    } 
+    </style>
+</head>
+
+<body>
+    <div class="container-fluid">
+        <div class="row justify-content-center">
+                <div class="col-12 col-md-5 p-1">
+                    <h2>Welcome back to Credi7</h3>
+                    <!-- <img src="https://credi7.com.mx/wp-content/uploads/2022/01/logo_credi_7.png" alt="" class="img-fluid"> -->
+                </div>
+                <!-- https://credi7.com.mx/wp-content/uploads/2022/01/logo_credi_7.png -->
+        </div>
+        <div class="row justify-content-center">
+            <div class="col-5 p-4 border white border-secundary rounded">
+                <div class="col-5 blue rounded p-2 mb-4">
+                    <img src="https://credi7.com.mx/wp-content/uploads/2022/01/logo_credi_7.png" alt="" class="img-fluid">
+                </div>
+            <!-- <div class="col-4 p-5 col-md-4 col-sm-10 col-10 border white border-secundary border-3 rounded mt-5 p-3"> -->
+            <form>
+                <div class="mb-3">
+                    <label class="form-label">Numero Financiamiento:</label>
+                    <input type="codigobloqueo" class="form-control">
+                </div>
+                <div class="row justify-content-center">
+                    <div class="col-3">
+                        <button type="submit" class="btn btn-primary">Buscar</button>
+                    </div>
+                </div>
+            </form>
+           </div>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
