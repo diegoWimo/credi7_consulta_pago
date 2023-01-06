55ce15ed652131ebcadcb68e31e109ecb9e31cdd
legolasblack <legolas.black.ds@gmail.com>
2023-01-06 16:21:49 -0600

    archivo de conexion modificado

--- www/modelo/Conexion.php
+++ www/modelo/Conexion.php
@@ -6,7 +6,7 @@ class Conectar{
     public static function conexion(){
     
         try {
-            $conexion=new PDO('mysql:host=localhost; dbname=credicontrol', 'root', '');
+            $conexion=new PDO('mysql:host=db; dbname=credicontrol', 'root', 'test');
             $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $conexion->exec("SET CHARACTER SET UTF8");
             return $conexion;
