27a409ebdd60314a6bf9b347977f74166dedd4f5
legolasblack <legolas.black.ds@gmail.com>
2023-01-06 00:46:02 -0600

    primera version para deploy

--- controlador/Buscador_Financiamiento_conrtrolador.php
+++ controlador/Buscador_Financiamiento_conrtrolador.php
@@ -4,12 +4,14 @@ require_once "../modelo/conexion.php";
 if (preg_match('/^[0-9]+$/', $_POST["buscar"])) {
     $conexion = Conectar::conexion();
 //$query ="SELECT C.nombre_cliente, F.Fecha_Financ FROM clientes WHERE nombre_cliente LIKE LOWER('%".$_POST["buscar"]."%')";
-    $query = "SELECT CLI.NOMBRE_CLIENTE ,FIN.FECHA_FINANC, FIN.IMEI, ART.DESCRIPCION_ART
+    $query = "SELECT CLI.NOMBRE_CLIENTE ,FIN.FECHA_FINANC, FIN.IMEI, ART.DESCRIPCION_ART, OPPAGO.CODIGO_BLOQUEO,OPPAGO.MONTO_ADEUDO,OPPAGO.OPCION_1
     FROM FINANCIAMIENTOS FIN 
     INNER JOIN CLIENTES CLI 
     ON FIN.NUM_CLIENTE = CLI.NUM_CLIENTE 
     INNER JOIN ARTICULOS ART
     ON FIN.CODIGO_ART= ART.CODIGO_ART
+    INNER JOIN OPCIONES_PAGO OPPAGO
+    ON OPPAGO.ORDEN_FINANC=FIN.ORDEN_FINANC
     WHERE FIN.ORDEN_FINANC= " . $_POST['buscar'] . "";
 //$query ="SELECT C.nombre_cliente, F.Fecha_Financ FROM clientes WHERE nombre_cliente LIKE LOWER('%".$_POST["buscar"]."%')";
 //$query ="SELECT * FROM clientes WHERE nombre_cliente LIKE LOWER('%".$_POST["buscar"]."%')";
@@ -42,27 +44,37 @@ WHERE ORDEN_FINANC=" . $_POST['buscar'] . "";
 }
 
 if ($resultado_consulta == null) {
-    echo "<h5>El codigo de financiemiento no coincide con algun financiamineto Activo, porfavor ingrese un codigo valido.</h5>";
+    echo "<h5>El codigo de financiemiento no coincide con algun financiamineto con adeudo, porfavor ingrese un codigo valido.</h5>";
 } else {?>
 
 <div class="row">
     <div class=col-6>
         <h5>Nombre:</h5>
-        <p><?php echo $resultado_consulta[0]["NOMBRE_CLIENTE"] ?></p>
+        <p><?php echo $resultado_consulta[0]["NOMBRE_CLIENTE"]; ?></p>
     </div>
     <div class=col-6>
         <h5>Fecha Venta:</h5>
-        <p><?php echo $resultado_consulta[0]["FECHA_FINANC"] ?></p>
+        <p><?php echo $resultado_consulta[0]["FECHA_FINANC"]; ?></p>
     </div>
 </div>
 <div class="row">
     <div class=col-6>
         <h5>IMEI:</h5>
-        <p><?php echo $resultado_consulta[0]["IMEI"] ?></p>
+        <p><?php echo $resultado_consulta[0]["IMEI"] ;?></p>
     </div>
     <div class=col-6>
-        <h5>Fecha Venta:</h5>
-        <p><?php echo $resultado_consulta[0]["DESCRIPCION_ART"] ?></p>
+        <h5>Modelo:</h5>
+        <p><?php echo $resultado_consulta[0]["DESCRIPCION_ART"]; ?></p>
+    </div>
+</div>
+<div class="row">
+    <div class=col-6>
+        <h5>Codigo de bloqueo:</h5>
+        <p><?php echo$resultado_consulta[0]["CODIGO_BLOQUEO"]; ?></p>
+    </div>
+    <div class=col-6>
+        <h5>Monto de adeudo:</h5>
+        <p>$ <?php echo $resultado_consulta[0]["MONTO_ADEUDO"]; ?></p>
     </div>
 </div>
 <div class="row">
@@ -72,7 +84,7 @@ if ($resultado_consulta == null) {
     </div>
     <div class=col-6>
         <h5>Monto de pago:</h5>
-        <p>$8,500</p>
+        <p>$ <?php echo $resultado_consulta[0]["OPCION_1"]; ?>.00</p>
     </div>
 </div>
 
